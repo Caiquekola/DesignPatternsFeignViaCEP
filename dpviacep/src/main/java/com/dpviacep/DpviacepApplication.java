d4e18dd3d8ffa8235e98470a9fbb9bee6e3b3261
Caique Augusto <caique.augusto@unsoft.com.br>
2026-09-23 19:28:54 -0300

    feat: springbootapp

---
+++ dpviacep/src/main/java/com/dpviacep/DpviacepApplication.java
@@ -0,0 +1,13 @@
+package com.dpviacep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DpviacepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DpviacepApplication.class, args);
+	}
+
+}
---
+++ dpviacep/src/test/java/com/dpviacep/DpviacepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dpviacep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DpviacepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
